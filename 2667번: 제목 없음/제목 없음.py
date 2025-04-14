623ab180d15302ed5d3d9ffb0e1c788e431934cb
stupidyoh <gattswet0926@gmail.com>
2025-04-14 09:45:07 +0900

    BOJ-EX: 2025. 4. 14. 오전 9:45:06

--- 2667번: 제목 없음/제목 없음.py
+++ 2667번: 제목 없음/제목 없음.py
@@ -1,17 +1,22 @@
 #  **************************************************************************  #
 #                                                                              #
 #                                                       :::    :::    :::      #
-#    Problem Number: 2693                              :+:    :+:      :+:     #
+#    Problem Number: 2667                              :+:    :+:      :+:     #
 #                                                     +:+    +:+        +:+    #
 #    By: gattswet0926 <boj.kr/u/gattswet0926>        +#+    +#+          +#+   #
 #                                                   +#+      +#+        +#+    #
-#    https://boj.kr/2693                           #+#        #+#      #+#     #
-#    Solved: 2025/04/10 09:32:39 by gattswet0926  ###          ###   ##.kr     #
+#    https://boj.kr/2667                           #+#        #+#      #+#     #
+#    Solved: 2025/04/10 09:43:56 by gattswet0926  ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
-t = int(input())
+n = int(input())
 
-for _ in range(t):
-    a = list(map(int, input().split()))
-    a = sorted(a)
-    print(a[-3])
+building_map = []
+for _ in range(n):
+    building_map.append(list(map(int, input().split())))
+
+visited = [[0 for _ in range(n)] for _ in range(n)]
+answer = []
+
+for h in range(n):
+    for v in range(n)
