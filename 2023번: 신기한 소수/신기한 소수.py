d269030036377dae8a7e043dceb535ab836edd76
stupidyoh <gattswet0926@gmail.com>
2025-04-24 12:55:43 +0900

    BOJ-EX: 2025. 4. 24. 오후 12:55:42

--- 2023번: 신기한 소수/신기한 소수.py
+++ 2023번: 신기한 소수/신기한 소수.py
@@ -13,4 +13,34 @@
 # 주어진 입력받기
 n = int(input())
 
-#  
+# 소수 판별 함수 정의
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+        
+    return True
+
+awesome_prime = []
+
+# 백트래킹
+def back():
+    if len(awesome_prime) == n:
+        print(int(''.join(map(str, awesome_prime))))
+        return
+    
+    # 0이 붙는 경우는 소수가 아니므로 제외
+    for i in range(1, 10):
+        awesome_prime.append(i)
+        # print(awesome_prime)
+        if is_prime(int(''.join(map(str, awesome_prime)))):
+            back()
+
+        awesome_prime.pop()
+
+back()
+
+
