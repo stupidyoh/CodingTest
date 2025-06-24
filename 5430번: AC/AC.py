cd36a258d7725939bba08c2c4d718e4ba5ba6878
stupidyoh <gattswet0926@gmail.com>
2025-06-24 21:49:30 +0900

    BOJ-EX: 2025. 6. 24. 오후 9:49:29

--- 5430번: AC/AC.py
+++ 5430번: AC/AC.py
@@ -12,6 +12,7 @@
 
 from collections import deque
 
+# 주어진 입력받기
 T = int(input())
 for _ in range(T):
     p = input()
@@ -19,16 +20,21 @@ for _ in range(T):
     integers = input()[1:-1].split(",")
     integers = deque(integers)
 
+    # []이 입력일 경우 별도 처리
     if n == 0:
         integers = []
 
+    # error 여부 및 뒤집힌 상태 초기화
     is_error = False
-    is_flipped = -1
+    is_flipped = -1 # 1: 뒤집힘 / -1: 안뒤집힘
 
+    # 각 함수 실행
     for func in p:
         if func == "R":
+            # 뒤집힘 상태 역전
             is_flipped = -is_flipped
         elif func == "D":
+            # []일 경우 error 처리
             if len(integers) < 1:
                 is_error = True
                 print("error")
@@ -39,6 +45,7 @@ for _ in range(T):
             # 뒤집혔으면 뒤에서 제거
             else:
                 integers.pop()
+    # 정답 출력
     if not is_error:
         if is_flipped == -1:
             print("[" + ",".join(integers) + "]")
